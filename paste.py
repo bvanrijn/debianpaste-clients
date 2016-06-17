04df9e5be6fa0eb3f7979197f1ba228d7fdd5f2a
Barend <bvanrijn@users.noreply.github.com>
2016-06-17 12:19:50 +0200

    GPL because #3

--- paste.py
+++ paste.py
@@ -4,7 +4,7 @@
 # Original code by:                        Copyright 2007-2011 Michael Gebetsroither <michael@mgeb.org>
 # Author of this fork (AoF):               Copyright 2016 Github user bvanrijn, <b.vanrijn@me.com>
 # License:                                 This file is licensed under the GPL v2+. Full license text in LICENSE
-# Modified original:                       Yes
+# Modified original:                       Yes, I forked the repository at commit 8adff71 and modified it
 # AoF started working on modified version: Thu Jun 16 21:08:56 2016 +0200
 #
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
